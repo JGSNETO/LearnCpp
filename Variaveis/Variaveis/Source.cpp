d6f68f12cd3747faf016040a20ba690652cf0277
José Neto <jgsnto.sd@gmail.com>
2022-03-02 10:13:47 -0300

    Variables

---
+++ Variaveis/Variaveis/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main()
+{
+	//Tipos de variáveis
+	int NumVidas = 5;
+	int Score = 1350;
+	float numero2 = 55.56f;
+	double numero3 = 44.09930;
+	char caractere = '3';
+	bool teste = true; 
+
+	//Constantes
+
+	const int desconto = 10; 
+
+
+	std::cout << "Numeros de vidas: " << NumVidas << std::endl;
+	std::cout << "Score: " << Score << std::endl;
+
+	system("PAUSE");
+
+/*Regras para declarar variáveis
+-Nome deve ser composto apenas por letras, números ou sublinhado;
+-Deve começar com uma letra ou sblinhado 
+-Não pode ser igual a nenhuma palavra reservada
+
+*/
+}
